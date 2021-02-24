e93a0d391cbbe610f7ef4cf11b88c4647d43f904
opq <achriste@hawaii.edu>
2021-02-23 16:50:32 -1000

    Add librosa, bump version.

---
+++ scripts/docs.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+cd ..
+
+if ! [[ -x "$(command -v pdoc3)" ]]; then
+  echo 'Error: pdoc3 is not installed.' >&2
+  exit 1
+fi
+
+set -o nounset
+set -o errexit
+set -o xtrace
+
+rm -rf docs/api_docs
+pdoc3 libquantum --overwrite --html --html-dir docs/api_docs -c show_type_annotations=True
