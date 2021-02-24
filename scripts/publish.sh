16dad8fd78bfef76c7e7e92fc380e69b6735aabe
opq <achriste@hawaii.edu>
2021-02-24 13:29:20 -1000

    Automatically create tags in git when publishing.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -20,6 +20,13 @@ set -o errexit
 set -o xtrace
 
 cd ..
+# Build the distributions
 python3 setup.py sdist bdist_wheel
 
+# Upload the distributions to PyPi
 twine upload -r pypi -u ${USER} -p ${PASS} --skip-existing dist/*
+
+# Create a git tag for this version
+VERSION="v$(python3 setup.py --version)"
+git tag -a ${VERSION} -m"Release ${VERSION}"
+git push origin ${VERSION}
