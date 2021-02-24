a86298711f2d35ac1143ca1a06382fac100b2753
milton <milton@isla.hawaii.edu>
2021-02-23 15:18:24 -1000

    More bits, from libwwz

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt", "r") as requirements_file:
+    requirements = list(map(lambda line: line.strip(), requirements_file.readlines()))
+    requirements = list(filter(lambda line: (not line.startswith("#")) and len(line) > 0, requirements))
+
+setup(name="libquantum",
+      version="1.0.0",
+      url='https://github.com/RedVoxInc/libquantum',
+      license='Apache',
+      author='RedVox',
+      author_email='dev@redvoxsound.com',
+      description='Library for implementing standardized time-frequency representations.',
+      packages=find_packages(include=[
+          "libquantum",
+          "plot_templates",
+      ],
+          exclude=['tests']),
+      long_description_content_type='text/markdown',
+      long_description=open('README.md').read(),
+      install_requires=requirements,
+      python_requires=">=3.6")
---
+++ tests/test_quantum.py
@@ -0,0 +1,13 @@
+"""
+This module contains unit tests for the WWZ.
+"""
+
+import unittest
+
+
+class TestQuantum(unittest.TestCase):
+    def test_q(self):
+        """
+        I don't do a whole lot yet...
+        """
+        self.assertTrue(True)
