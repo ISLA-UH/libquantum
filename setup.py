12434cae8cb95a11cccedb67061b68095c8fa6a8
milton <milton@isla.hawaii.edu>
2021-08-03 16:21:30 -1000

    Verified compatibility with Python 3.9. Updated requirements.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt", "r") as requirements_file:
     requirements = list(filter(lambda line: (not line.startswith("#")) and len(line) > 0, requirements))
 
 setup(name="libquantum",
-      version="1.2.0",
+      version="1.2.1",
       url='https://github.com/RedVoxInc/libquantum',
       license='Apache',
       author='RedVox',
