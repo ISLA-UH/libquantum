52756c2002e358492ca630650bf006c36fe29d5d
milton <milton@isla.hawaii.edu>
2022-05-13 11:32:08 -1000

    Update the math, then roll out to code. M = 3/4 pi

--- examples_scipy/s00_stft_tone_tests.py
+++ examples_scipy/s00_stft_tone_tests.py
@@ -69,24 +69,28 @@ if __name__ == "__main__":
     """
 
     print('Tone synthetic')
-    order_number_input = 12
+    order_number_input = 1
     EVENT_NAME = "Tone Test"
     station_id_str = 'Dyad_Sig'
     sig_is_pow2 = True
 
     sig_frequency_hz = 50.
-    sig_sample_rate_hz = 800.
-    sig_duration_nominal_s = 5.
+    sig_sample_rate_hz = 1000.
+    sig_duration_nominal_s = 10.
 
-    ref_frequency_hz = sig_frequency_hz/4.
+    ref_frequency_hz = 1.
     ref_period_nominal_s = 1./ref_frequency_hz
 
     ref_period_points_ceil_log2, ref_period_points_ceil_pow2, ref_period_ceil_pow2_s = \
         duration_ceil(sample_rate_hz=sig_sample_rate_hz, time_s=ref_period_nominal_s)
 
-    # Convert to function calls
+    # TODO: Convert to function calls
     # Gabor Multiplier
-    sig_cycles_M = 2*np.sqrt(2*np.log(2))*order_number_input
+    m_factor = 2*np.sqrt(2*np.log(2))
+    sig_cycles_M = m_factor*order_number_input
+    print("Exact M factor:", m_factor)
+    print("Approximate factor:", 3*np.pi/4)
+
     sig_duration_min_s = sig_cycles_M/sig_frequency_hz
 
     # Verify request
@@ -96,14 +100,14 @@ if __name__ == "__main__":
     sig_duration_points_log2, sig_duration_points_pow2, sig_duration_pow2_s = \
         duration_ceil(sample_rate_hz=sig_sample_rate_hz, time_s=sig_duration_nominal_s)
 
-    # Construct synthetic tone with max unit rms amplitude
+    # Construct synthetic tone with 2^^n points and max unit rms amplitude
     mic_sig_epoch_s = np.arange(sig_duration_points_pow2) / sig_sample_rate_hz
     mic_sig = np.sqrt(2)*np.sin(2*np.pi*sig_frequency_hz*mic_sig_epoch_s)
     # Add noise
     mic_sig += synthetics.white_noise_fbits(sig=mic_sig, std_bit_loss=4.)
-    # # Taper at spectrogram
-    # mic_sig *= utils.taper_tukey(mic_sig_epoch_s, fraction_cosine=0.1)
-    # Antialias
+    # Taper before AA
+    mic_sig *= utils.taper_tukey(mic_sig_epoch_s, fraction_cosine=0.1)
+    # Antialias (AA)
     synthetics.antialias_halfNyquist(mic_sig)
 
     # STFT spectral resolution, or linear S transform spectral resolution
